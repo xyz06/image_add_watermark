7438c57341d63fdc79a524c521d2f15acc5ee594
xyz06 <1056156342@qq.com>
2020-05-08 10:41:07 +0800

    modify server_image.py and readme

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 ## 图片加水印
 
-运行server_image.py文件,启动服务器
+###下载
+在命令窗口(Windows 使用 win+R 调出 cmd 运行框)使用以下命令，下载
 
-浏览器输入[http://127.0.0.1:5000/](http://127.0.0.1:5000)
+    C:\Users\Administrator>cd e:
+	E:>git clone https://github.com/xyz06/image_add_watermark.git
+下载完成就会有image_add_watermark文件夹	
+    
+
+##
+###运行
+进入image_add_watermark文件夹，运行server_image.py文件启动服务
+
+    E:>cd image_add_watermark
+    E:\image_add_watermark>python server_image.py
+
+出现以下内容表示启动成功
+<img src="start.png" width="80%" height="20%" />
+
+##
+###使用
+
+浏览器输入[http://127.0.0.1:5000/](http://127.0.0.1:5000),显示主页
+
 <img src="index.png" width="80%" height="50%" />
 
 当没有上传图片或文字，就会跳转到提示错误页
--- server_image.py
+++ server_image.py
@@ -17,11 +17,11 @@ def upload():
     text = flask.request.form.get("text")
     color = flask.request.form.get("color")
     if not img or not text:
-        return flask.render_template("alert.html",alert="No img or no text")
+        return flask.render_template("alert.html", alert="No img or no text")
     if not color or color not in ['white', 'blue', 'red', 'orange', 'yellow', 'green', 'black']:
         color = "white"
-
-    dir_path = os.path.abspath(os.path.dirname(__file__)+"/static/image")
+    dir_path = os.path.dirname(os.path.abspath(__file__))+"/static/image"
+    # print(dir_path)
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
     img_path = dir_path +"\\"+ img.filename
@@ -34,12 +34,12 @@ def upload():
 def download():
     if "img" in flask.request.values:
         img = urllib.parse.quote(flask.request.values.get("img"))
-        dir_path = os.path.abspath(os.path.dirname(__file__)) + "/static/"
-        print(dir_path)
+        dir_path = os.path.dirname(os.path.abspath(__file__)) + "/static/"
+        # print(dir_path)
         return flask.send_from_directory(dir_path, img, as_attachment=True)
     else:
-        return flask.render_template("alert.html", "No image")
+        return flask.render_template("alert.html", alert="No image")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
